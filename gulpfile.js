dbfe62d715aaa88fad52be075004c18bfe87b2a4
Lymke <mickael.rolo@gmail.com>
2016-10-21 16:42:26 +0200

    Replace tabs by spaces for indentation

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@
 // Gulp and gulp plugins
 /////////////////////////////
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	extend = require('extend'),
+    gutil = require('gulp-util'),
+    extend = require('extend'),
     argv = require('minimist')(process.argv),
     fs = require("fs"),
     clean = require('gulp-clean'),
@@ -17,8 +17,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     htmlmin = require('gulp-htmlmin'),
-	removeHtmlComments = require('gulp-remove-html-comments'),
-	cleanCSS = require('gulp-clean-css');
+    removeHtmlComments = require('gulp-remove-html-comments'),
+    cleanCSS = require('gulp-clean-css');
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     jsonminify = require('gulp-jsonminify'),
@@ -46,93 +46,91 @@ gulp.task('clean', function () {
 //CSS,  Run sass to create the file ./deploiement/style.min.css and minify css from sources.json
 gulp.task('css-sass',['clean'], function () {
     return gulp.src(oSources.sass,{base: '.'})
-            .pipe(sourcemaps.init())
-            .pipe(sass({outputStyle: 'compressed'})
-			.on('error', sass.logError))
-            .pipe(sourcemaps.write())
-			.pipe(gp_concat('style.min.css'))
-            .pipe(gulp.dest(PATH_DEPLOIEMENT));
+               .pipe(sourcemaps.init())
+               .pipe(sass({outputStyle: 'compressed'})
+               .on('error', sass.logError))
+               .pipe(sourcemaps.write())
+               .pipe(gp_concat('style.min.css'))
+               .pipe(gulp.dest(PATH_DEPLOIEMENT));
 });
 
 gulp.task('css-minify',['clean'], function() {
   return gulp.src(oSources.css)
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(PATH_DEPLOIEMENT));
+             .pipe(cleanCSS({compatibility: 'ie8'}))
+             .pipe(gulp.dest(PATH_DEPLOIEMENT));
 });
 
 //JSON : minify json from ./config/sources.json and put the result in ./deploiement with the same tree (modules/nommodule/data..)
 gulp.task('json-minify',['clean'], function () {
     return gulp.src(oSources.json, {base: '.'})
-            .pipe(jsonminify())
-            .pipe(gulp.dest(PATH_DEPLOIEMENT));
+               .pipe(jsonminify())
+               .pipe(gulp.dest(PATH_DEPLOIEMENT));
 });
 
 //HTML : minify html files from ./config/sources.json and put the result in ./deploiement  with the same tree
 gulp.task('html-minify',['clean'], function () {
     return gulp.src(oSources.html,{base: '.'})
-			.pipe(removeHtmlComments())
-            .pipe(htmlmin({collapseWhitespace: true}))
-            .pipe(gulp.dest(PATH_DEPLOIEMENT));
+               .pipe(removeHtmlComments())
+               .pipe(htmlmin({collapseWhitespace: true}))
+               .pipe(gulp.dest(PATH_DEPLOIEMENT));
 });
 
 //JS : minify and concat JS from ./config/sources.json and create the file ./deploiement/script.min.js
 gulp.task('js-minify',['clean'], function (callback) {
-	
-	
-	return gulp.src(oSources.js)
-				.pipe(jshint())
-				.pipe(gp_concat('script.js'))
-				.pipe(ngAnnotate())
-				.pipe(uglify())
-				.pipe(rename({suffix: '.min'}))
-				.pipe(gulp.dest(PATH_DEPLOIEMENT ));
-	
+     return gulp.src(oSources.js)
+                .pipe(jshint())
+                .pipe(gp_concat('script.js'))
+                .pipe(ngAnnotate())
+                .pipe(uglify())
+                .pipe(rename({suffix: '.min'}))
+                 .pipe(gulp.dest(PATH_DEPLOIEMENT ));
+     
 });
 
 //PICTURES : copy pictures from config/sources.json in deploiement
 gulp.task('images', function () {
     gulp.src(oSources.images).pipe(imageop({
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true
-    }))
-            .pipe(gulp.dest(sIMAGESDestination))
-            .on('end', function () {
-                console.log('task images end');
-            })
-            .on('error', function () {
-                console.log('task images error');
-            });
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest(sIMAGESDestination))
+        .on('end', function () {
+             console.log('task images end');
+        })
+        .on('error', function () {
+             console.log('task images error');
+        });
 });
 
 //COPY-INDEX : Create the deploiement/index.html and inject dependances of local css, js and external librairies
 gulp.task('copy-index',['clean'], function () {
     gulp.src(PATH_INDEX)
-            .pipe(rename({basename: 'index', extname: ".html"}))
-            .pipe(gulp.dest(PATH_DEPLOIEMENT));
+        .pipe(rename({basename: 'index', extname: ".html"}))
+        .pipe(gulp.dest(PATH_DEPLOIEMENT));
 
 });
 
 gulp.task('inject', ['copy-index','css-sass','js-minify'], function () {
-	
-	sExterneJs = '\n';
-	for(sSource in oSources.dist.js){
-		sExterneJs += '     <script src="'+oSources.dist.js[sSource]+'"></script>' + '\n';
-	}
-	
-	sExterneCSS = '\n';
-	for(sSource in oSources.dist.css){
-		sExterneCSS += '     <style  rel="stylesheet" href="'+oSources.dist.css[sSource]+'"></style>' + '\n';
-	}
-		
-	gulp.src(PATH_DEPLOIEMENT+'index.html')
-		.pipe(injectString.replace('##title##',oProject.title))
-		.pipe(injectString.replace('##appName##',oProject.appName))
-		.pipe(injectString.replace('##description##',oProject.description))
-		.pipe(injectString.replace('##dist-js##',sExterneJs))
-		.pipe(injectString.replace('##dist-css##',sExterneCSS))
-		.pipe(inject(gulp.src(['**/*.js','**/*.css'], {read: false,cwd: PATH_DEPLOIEMENT}), {relative: true}))
-		.pipe(gulp.dest(PATH_DEPLOIEMENT));
+     
+     sExterneJs = '\n';
+     for(sSource in oSources.dist.js){
+          sExterneJs += '     <script src="'+oSources.dist.js[sSource]+'"></script>' + '\n';
+     }
+     
+     sExterneCSS = '\n';
+     for(sSource in oSources.dist.css){
+          sExterneCSS += '     <style  rel="stylesheet" href="'+oSources.dist.css[sSource]+'"></style>' + '\n';
+     }
+          
+     gulp.src(PATH_DEPLOIEMENT+'index.html')
+         .pipe(injectString.replace('##title##',oProject.title))
+         .pipe(injectString.replace('##appName##',oProject.appName))
+         .pipe(injectString.replace('##description##',oProject.description))
+         .pipe(injectString.replace('##dist-js##',sExterneJs))
+         .pipe(injectString.replace('##dist-css##',sExterneCSS))
+         .pipe(inject(gulp.src(['**/*.js','**/*.css'], {read: false,cwd: PATH_DEPLOIEMENT}), {relative: true}))
+         .pipe(gulp.dest(PATH_DEPLOIEMENT));
 });
 
 /////////////////////////////
@@ -145,45 +143,45 @@ gulp.task('inject', ['copy-index','css-sass','js-minify'], function () {
 // Macro tasks
 /////////////////////////////
 gulp.task('default',function(){
-	
-	styleMenu = gutil.colors.bgBlue.white;
-	
-	gutil.log(styleMenu('Bienvenue !!!^^!!!'));
-	gutil.log(styleMenu('Commencez par préparer le déploiement, puis déployez sur votre serveur :'));
-	gutil.log(styleMenu('1) gulp prepare-deploiement'));
-	gutil.log(styleMenu('2) gulp deploy --env xxx'));
-	
+     
+     styleMenu = gutil.colors.bgBlue.white;
+     
+     gutil.log(styleMenu('Bienvenue !!!^^!!!'));
+     gutil.log(styleMenu('Commencez par préparer le déploiement, puis déployez sur votre serveur :'));
+     gutil.log(styleMenu('1) gulp prepare-deploiement'));
+     gutil.log(styleMenu('2) gulp deploy --env xxx'));
+     
 });
 
 gulp.task('prepare-deploiement',['css-sass','css-minify','json-minify','html-minify','inject']);
 
 gulp.task('deploy', function () {
 
-	oDeploy = {
-		"progress": false,
-		"incremental": true,
-		"relative": true,
-		"emptyDirectories": true,
-		"recursive": true,
-		"clean": true,
-		"chmod": "ugo=rwX",
-		"root" : __dirname + '/' + PATH_DEPLOIEMENT
-	};
-	
-	oConfigConnexion = require('./gulp/config/deploy.json')[argv.env];
-	
-	if(oConfigConnexion == undefined){
-		gutil.log(gutil.colors.bgRed.white('Config connexion undefined'));
-		return false;
-	}else{
-		return gulp.src(PATH_DEPLOIEMENT + '**/*')
-            .pipe(gulpif((argv.production || (argv.env && argv.env == 'production')),
-                    prompt.confirm({
-                        message: 'Etes-vous sûr de vouloir publier en production ?',
-                        default: false
-                    })
-            ))
-            .pipe(rsync(extend(oDeploy,oConfigConnexion)));
-	}
+     oDeploy = {
+          "progress": false,
+          "incremental": true,
+          "relative": true,
+          "emptyDirectories": true,
+          "recursive": true,
+          "clean": true,
+          "chmod": "ugo=rwX",
+          "root" : __dirname + '/' + PATH_DEPLOIEMENT
+     };
+     
+     oConfigConnexion = require('./gulp/config/deploy.json')[argv.env];
+     
+     if(oConfigConnexion == undefined){
+          gutil.log(gutil.colors.bgRed.white('Config connexion undefined'));
+          return false;
+     }else{
+          return gulp.src(PATH_DEPLOIEMENT + '**/*')
+                     .pipe(gulpif((argv.production || (argv.env && argv.env == 'production')),
+                           prompt.confirm({
+                                message: 'Etes-vous sûr de vouloir publier en production ?',
+                                default: false
+                          })
+                     ))
+                     .pipe(rsync(extend(oDeploy,oConfigConnexion)));
+     }
     
 });
